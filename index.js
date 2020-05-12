fc2a646ec5a88610eb5fa040b89f441a289dfad0
tokorom <tokorom@gmail.com>
2020-05-12 11:49:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const core = require('@actions/core');
-const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    console.log('start')
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+    console.log('finish')
   } 
   catch (error) {
     core.setFailed(error.message);
