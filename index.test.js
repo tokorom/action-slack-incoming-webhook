dc9f58813ae2e3b0fa0e485c194481170fd6674c
tokorom <tokorom@gmail.com>
2021-05-06 17:21:48 +0900

    Add a test

--- index.test.js
+++ index.test.js
@@ -47,6 +47,39 @@ test('test create message with attachments', () => {
   expect(message.attachments[0].color).toEqual('good')
 })
 
+test('test create message with attachments with multiple lines', () => {
+  const attachments = `[
+  {
+      "color": "good",
+      "author_name": "foo",
+      "author_icon": "https://avatars2.githubusercontent.com/u/629993?v=4",
+      "fields": [
+        {
+          "title": "Commit Message",
+          "value": "Merge pull request #2 from foo/test
+  
+  複数行のcommit messages　"
+        },
+        {
+          "title": "GitHub Actions URL",
+          "value": "https://github.com/foo/github-actions-sandbox/actions/runs/103198369"
+        },
+        {
+          "title": "Compare URL",
+          "value": "https://github.com/foo/github-actions-sandbox/compare/97aab62ea3dd...feed685e572a"
+        }
+      ]
+    }
+  ]`
+
+  setupInputs({
+    'attachments': attachments,
+  })
+
+  const message = createMessage()
+  expect(message.attachments[0].fields[1].value).toEqual('https://github.com/foo/github-actions-sandbox/actions/runs/103198369')
+})
+
 test('test create message with thread_ts', () => {
   setupInputs({
     'thread_ts': '1571797440.006700',
